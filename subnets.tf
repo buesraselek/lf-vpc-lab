b6d3ed73fcbba16fff3f1065d6b33a953ab6fa28
buesraselek <buesra.selek@cloudreach.com>
2022-10-03 15:19:22 +0200

    create my own work with terraform lf-vpc-lab

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "public-1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "192.168.1.0/24"
+  map_public_ip_on_launch = "true"
   availability_zone="eu-central-1a"
   tags = {
     Name = "public-subnet-1"
@@ -9,6 +10,7 @@ resource "aws_subnet" "public-1" {
 resource "aws_subnet" "public-2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "192.168.2.0/24"
+  map_public_ip_on_launch = "true"
   availability_zone="eu-central-1b"
   tags = {
     Name = "public-subnet-2"
@@ -18,6 +20,7 @@ resource "aws_subnet" "public-2" {
 resource "aws_subnet" "public-3" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "192.168.3.0/24"
+  map_public_ip_on_launch = "true"
   availability_zone="eu-central-1c"
   tags = {
     Name = "public-subnet-3"
